a4da387ca71b97d6ab6847db5bec7e28747edcfd
Andrew Paradi <me@andrewparadi.com>
2017-03-25 00:05:28 -0400

    [CLOSES #2] Git symlinks .gitconfig to ~/ * bootstrap.sh, progress on brew cask installs * macOS_security, zsh, and Homebrew

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ ! -x /usr/bin/gcc ]]; then
+  xcode-select --install
+fi
+
+if [[ ! -x ~/.homebrew/bin/brew ]]; then
+  mkdir -p ~/.homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/.homebrew
+fi
+
+export PATH=$PATH:~/.homebrew/sbin:~/.homebrew/bin
+
+if [[ ! -x ~/.homebrew/bin/git ]]; then
+  brew install git
+fi
+
+if [[ ! -x ~/.homebrew/bin/ansible ]]; then
+  brew install ansible
+fi
+
+cd ~/.files/ansible && ansible-playbook --ask-sudo-pass -i inventories/macbox/hosts plays/provision/mac_dev.yml -e "home=/Users/me"
