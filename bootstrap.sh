c9e7417467730c3891e9071069d1f5682e889428
Andrew Paradi <me@andrewparadi.com>
2017-04-10 00:09:58 -0400

    [CLOSES #3] autodmg to homebrew cask
    * mac_dev play for work computer, works with different homebrew
    
    * .macos refactored and added to boostrap
    * Now runs core firewall, localhost name, and other settings before going online
    
    * enforce {HOME} paths for all commands, must be run in ~/.files
    
    * Refactored after testing on clean install

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+${HOME}/.files/ansible/roles/macos/files/custom.macos
+
 if [[ ! -x /usr/bin/gcc ]]; then
   xcode-select --install
 fi
@@ -10,7 +12,7 @@ if [[ ! -x ~/.homebrew/bin/brew ]]; then
   mkdir -p ~/.homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C ~/.homebrew
 fi
 
-export PATH=$PATH:~/.homebrew/sbin:~/.homebrew/bin
+export PATH=~/.homebrew/sbin:~/.homebrew/bin:$PATH
 
 if [[ ! -x ~/.homebrew/bin/git ]]; then
   brew install git
@@ -20,4 +22,4 @@ if [[ ! -x ~/.homebrew/bin/ansible ]]; then
   brew install ansible
 fi
 
-cd ~/.files/ansible && ansible-playbook --ask-sudo-pass -i inventories/macbox/hosts plays/provision/mac_core.yml -e "home=/Users/me mas_email= mas_password="
+cd ${HOME}/.files/ansible && ansible-playbook --ask-sudo-pass -i inventories/macbox/hosts plays/provision/mac_core.yml -e "home=${HOME} mas_email= mas_password="
