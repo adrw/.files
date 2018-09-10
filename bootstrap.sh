ec2459d8ed9e5a5e70882d04b307a22e9b0da82f
Andrew Paradi <account+github@adrw.ch>
2018-09-10 14:02:17 -0400

    mac_test

--- bootstrap.sh
+++ bootstrap.sh
@@ -149,7 +149,7 @@ function mac_install_dependencies {
 function mac_scripts {
   status a "scripts | ${PLAY} @ ${INVENTORY}"
   case "${PLAY}" in
-  "mac_core"|"mac_square"|"mac_dev"|"mac_clean")
+  "mac_core"|"mac_square"|"mac_dev"|"mac_clean"|"mac_test")
     run_script ${SCRIPTS}/custom.macos
     run_script ${SCRIPTS}/.macos
     if [[ $(csrutil status) != *enabled* ]]; then
@@ -188,13 +188,10 @@ function mac_bootstrap {
 
   status a "ansible-playbook | ${PLAY} @ ${INVENTORY}"
   case "${PLAY}" in
-  "mac_core"|"mac_square"|"mac_dev"|"mac_clean")
+  "mac_core"|"mac_square")
     cd "${MAIN_DIR}/ansible" && ansible-playbook --ask-become-pass --ask-vault-pass -i inventories/${INVENTORY} plays/provision/${PLAY}.yml -e "home=${HOME} user_name=${USER_NAME} homebrew_prefix=${HOMEBREW_DIR} homebrew_install_path=${HOMEBREW_INSTALL_DIR} mas_email=${MAS_EMAIL} mas_password=${MAS_PASSWORD}"
     ;;
-  "mac_etchost_no_animate")
-    cd "${MAIN_DIR}/ansible" && ansible-playbook --ask-become-pass -i inventories/${INVENTORY} plays/provision/${PLAY}.yml -e "home=${HOME} user_name=${USER_NAME} homebrew_prefix=${HOMEBREW_DIR} homebrew_install_path=${HOMEBREW_INSTALL_DIR} mas_email=${MAS_EMAIL} mas_password=${MAS_PASSWORD}"
-    ;;
-  "mac_jekyll")
+  "mac_etchost_no_animate"|"mac_jekyll"|"mac_clean"|"mac_test")
     cd "${MAIN_DIR}/ansible" && ansible-playbook --ask-become-pass -i inventories/${INVENTORY} plays/provision/${PLAY}.yml -e "home=${HOME} user_name=${USER_NAME} homebrew_prefix=${HOMEBREW_DIR} homebrew_install_path=${HOMEBREW_INSTALL_DIR} mas_email=${MAS_EMAIL} mas_password=${MAS_PASSWORD}"
     ;;
   *)
