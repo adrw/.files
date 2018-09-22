05202499b32fe071ff7545d0fea21e48a40a30b9
Andrew Paradi <account+github@adrw.ch>
2018-09-22 19:43:39 -0400

    Audiobooks documentation. Logging partially fixed in bootstrap. Interactive mode started

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,85 +1,60 @@
-# By Andrew Paradi | Source at https://github.com/adrw/.files
 #!/usr/bin/env bash
+# By Andrew Paradi | Source at https://github.com/adrw/.files
 
 # set up bash to handle errors more aggressively - a "strict mode" of sorts
 set -e # give an error if any command finishes with a non-zero exit code
 set -u # give an error if we reference unset variables
 set -o pipefail # for a pipeline, if any of the commands fail with a non-zero exit code, fail the entire pipeline with that exit code
 
-function status() {
-  Reset="$(tput sgr0)"       # Text Reset
-  Red="$(tput setaf 1)"          # Red
-  Green="$(tput setaf 2)"        # Green
-  Blue="$(tput setaf 4)"         # Blue
-  div="********************************************************************************"
-  if [ "$#" -lt 3 ]; then   # if no name override passed in, take name "ap" if $0 is status, $0 otherwise
-    [ $(basename "${0}") = "status" ] && scriptname="ap" || scriptname=$(basename "${0}")
-  else
-    scriptname="${3}"
-  fi
-  case "${1}" in
-    a)        echo ""; echo "${Blue}<|${scriptname:0:1}${Reset} [ ${2} ] ${div:$((${#2}+9))}" ;;
-    b)        echo "${Green}ok: [ ${2} ] ${div:$((${#2}+9))}${Reset}" ;;
-    s|status) echo "${Blue}<|${scriptname:0:1}${Reset} [ ${2} ] ${div:$((${#2}+9))}" ;;
-    t|title)  echo "${Blue}<|${scriptname}${Reset} [ ${2} ] ${div:$((${#2}+8+${#scriptname}))}" ;;
-    e|err)    echo "${Red}fatal: [ ${2} ] ${div:$((${#2}+12))}${Reset}" ;;
-  esac
+# ADRW Logging
+Reset="$(tput sgr0)"            # Text Reset
+Red="$(tput setaf 1)"           # Red
+Green="$(tput setaf 2)"         # Green
+Yellow="$(tput setaf 3)"        # Yellow
+Blue="$(tput setaf 4)"          # Blue
+_start="$Blue<|$Reset"
+_end="$Blue|>$Reset"
+
+function FATAL {
+  echo "${_start}[$(date +'%Y-%m-%d %T')][${Red}FATAL${Reset}]${_end} $*"
 }
 
-function adrwl() {
-  Reset="$(tput sgr0)"       # Text Reset
-  Red="$(tput setaf 1)"          # Red
-  Green="$(tput setaf 2)"        # Green
-  Yellow="$(tput setaf 3)"          # Yellow
-  Blue="$(tput setaf 4)"         # Blue
-
-  _labels=""
-  _status=""
-  _timestamp="[$(date +'%Y-%m-%d %T')]"
-  _color=$Reset
-  _start="$Blue<|$Reset"
-  _end="$Blue|>$Reset"
-
-  while getopts "h?cdefitws:l:" opt; do
-    case "$opt" in
-      h|\?)      usage  ;;
-      c)        _timestamp="" ;;      # clean mode (no timestamp)
-      l)        _labels+="[$OPTARG]" ;;
-      s)        _status+="$OPTARG" ;;
-      f)        _status="FATAL"; _color=$Red ;;
-      e)        _status="ERROR"; _color=$Red ;;
-      w)        _status="WARN"; _color=$Yellow  ;;
-      i)        _status="INFO"; _color=$Green ;;
-      d)        _status="DEBUG"; _color=$Blue  ;;
-      t)        _status="TRACE"; _color=$Blue  ;;
-    esac
-  done
-  shift $((OPTIND-1));
-  _content="$*"
-
-  [[ ! -z $_status ]] && _status="[${_color}${_status}${Reset}]"
-  echo "${_start}${_timestamp}${_status}${_labels} ${_content}"
+function ERROR {
+  echo "${_start}[$(date +'%Y-%m-%d %T')][${Red}ERROR${Reset}]${_end} $*"
 }
 
-alias FATAL="adrwl -f"
-alias ERROR="adrwl -e"
-alias WARN="adrwl -w"
-alias INFO="adrwl -i"
-alias DEBUG="adrwl -d"
-alias TRACE="adrwl -t"
+function WARN {
+  echo "${_start}[$(date +'%Y-%m-%d %T')][${Yellow}WARN${Reset}]${_end} $*"
+}
+
+function INFO {
+  echo "${_start}[$(date +'%Y-%m-%d %T')][${Green}INFO${Reset}]${_end} $*"
+}
+
+function DEBUG {
+  echo "${_start}[$(date +'%Y-%m-%d %T')][${Blue}DEBUG${Reset}]${_end} $*"
+}
+
+function TRACE {
+  echo "${_start}[$(date +'%Y-%m-%d %T')][${Blue}Trace${Reset}]${_end} $*"
+}
+
+function LOG {
+  echo "${_start}[$(date +'%Y-%m-%d %T')][${Red}status${Reset}]${_end} $*"
+}
 
 function safe_download {
   timestamp="`date '+%Y%m%d-%H%M%S'`"
   if [ ! -f "$1" ]; then
-    status a "Download ${1}"
+    LOG a "Download ${1}"
     curl -s -o $1 $2
-    status b "Download ${1}"
+    LOG b "Download ${1}"
   else
-    status a "Update ${1}"
+    LOG a "Update ${1}"
     mv $1 $1.$timestamp
     curl -s -o $1 $2
     if diff -q "$1" "$1.$timestamp" > /dev/null; then rm $1.$timestamp; fi
-    status b "Update ${1}"
+    LOG b "Update ${1}"
   fi
 }
 
@@ -91,9 +66,9 @@ function run_script {
   exec=$*
   script=$1
   name=$(basename ${script})
-  status a "${name}"
+  LOG a "${name}"
   ${exec}
-  status b "${name}"
+  LOG b "${name}"
 }
 
 function usage {
@@ -126,9 +101,7 @@ EOF
 }
 
 function secure_hostname_network {
-  INFO "🔐  Secure network and custom host name"
-  read -p "Enter name for your Mac: " MAC_NAME
-  echo "  - MAC_NAME ${MAC_NAME}"
+  DEBUG "🔐  Secure network and change computer name: $(hostname) => ${COMPUTER_NAME}"
   # randomize MAC address
   sudo ifconfig en0 ether $(openssl rand -hex 6 | sed 's%\(..\)%\1:%g; s%.$%%')
 
@@ -136,10 +109,10 @@ function secure_hostname_network {
   networksetup -setairportpower en0 off
 
   # set computer name (as done via System Preferences → Sharing)
-  sudo scutil --set ComputerName "${MAC_NAME}"
-  sudo scutil --set HostName "${MAC_NAME}"
-  sudo scutil --set LocalHostName "${MAC_NAME}"
-  sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$MAC_NAME"
+  sudo scutil --set ComputerName "${COMPUTER_NAME}"
+  sudo scutil --set HostName "${COMPUTER_NAME}"
+  sudo scutil --set LocalHostName "${COMPUTER_NAME}"
+  sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPUTER_NAME"
 
   # enable firewall, logging, and stealth mode
   sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 1
@@ -155,49 +128,49 @@ function secure_hostname_network {
   networksetup -setairportpower en0 on
 
   sleep 5
-  DEBUG "🔐  Host Name: ${MAC_NAME}. Firewall: On."
+  INFO "🔐  Computer Name: ${COMPUTER_NAME}. Firewall: On."
 }
 
 function mac_install_dependencies {
-  status a "xcode-select, git, homebrew, ansible"
+  LOG a "xcode-select, git, homebrew, ansible"
 
   # todo? replace with https://github.com/elliotweiser/ansible-osx-command-line-tools
   if ! xcode-select -p 2> /dev/null; then
-    status a "Install xcode-select (Command Line Tools)"
+    LOG a "Install xcode-select (Command Line Tools)"
     xcode-select --install
-    status b  "Install xcode-select (Command Line Tools)"
+    LOG b  "Install xcode-select (Command Line Tools)"
   fi
 
   if [[ ! -x "${HOMEBREW_DIR}/bin/brew" ]]; then
-    status a "Install Homebrew"
+    LOG a "Install Homebrew"
     mkdir -p ${HOMEBREW_DIR} && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOMEBREW_DIR
-    status b "Install Homebrew"
+    LOG b "Install Homebrew"
   fi
 
   export PATH=${HOMEBREW_DIR}/sbin:${HOMEBREW_DIR}/bin:${PATH}
 
   if [[ ! -x ${HOMEBREW_DIR}/bin/git ]]; then
-    status a "Install Git"
+    LOG a "Install Git"
     brew install git
-    status b "Install Git"
+    LOG b "Install Git"
   fi
 
   if [[ ! -x ${HOMEBREW_DIR}/bin/ansible ]]; then
-    status a "Install Ansible"
+    LOG a "Install Ansible"
     brew install ansible
-    status b "Install Ansible"
+    LOG b "Install Ansible"
   fi
 
-  status b "xcode-select, git, homebrew, ansible"
+  LOG b "xcode-select, git, homebrew, ansible"
 }
 
 function mac_scripts {
-  status a "scripts | ${PLAY} @ ${INVENTORY}"
+  LOG a "scripts | ${PLAY} @ ${INVENTORY}"
   case "${PLAY}" in
   "mac_core"|"mac_square"|"mac_dev"|"mac_clean"|"mac_test_full"|"mac_test_short")
     run_script ${SCRIPTS}/custom.macos
     run_script ${SCRIPTS}/.macos
-    if [[ $(csrutil status) != *enabled* ]]; then
+    if [[ $(csrutil LOG) != *enabled* ]]; then
       run_script ${SCRIPTS}/homecall.sh fixmacos
     fi
     ;;
@@ -205,67 +178,66 @@ function mac_scripts {
     run_script ${SCRIPTS}/no_animate.macos
     ;;
   *)
-    status e "no scripts"
+    LOG e "no scripts"
   esac
-  status b "scripts | ${PLAY} @ ${INVENTORY}"
+  LOG b "scripts | ${PLAY} @ ${INVENTORY}"
 }
 
 function mac_bootstrap {
-  status a "Bootstrap Script"
+  LOG a "Bootstrap Script"
 
   mac_install_dependencies
 
-  status a "git/.files -> ${MAIN_DIR}"
+  LOG a "git/.files -> ${MAIN_DIR}"
   if [[ ! -d ${MAIN_DIR} ]]; then
-    status a "Clone .files"
+    LOG a "Clone .files"
     git clone https://github.com/adrw/.files.git ${MAIN_DIR}
-    status b "Clone .files"
+    LOG b "Clone .files"
   elif [[ "${TEST}" == false ]]; then
     # TODO Delete headless mode
-    status a "Decapitate .files (headless mode)"
-    cd ${MAIN_DIR}
-    git fetch --all
-    git reset --hard origin/master
-    git checkout origin/master
-    status b "Decapitate .files (headless mode)"
+    LOG a "Decapitate .files (headless mode)"
+    # cd ${MAIN_DIR}
+    # git fetch --all
+    # git reset --hard origin/master
+    # git checkout origin/master
+    # LOG b "Decapitate .files (headless mode)"
   fi
-  status b "git/.files -> ${MAIN_DIR}"
+  LOG b "git/.files -> ${MAIN_DIR}"
 
-  status a "ansible-playbook | ${PLAY} @ ${INVENTORY}"
-  USER_GROUP=$(id -Gn $USER_NAME | cut -d " " -f1)
+  LOG a "ansible-playbook | ${PLAY} @ ${INVENTORY}"
   case "${PLAY}" in
   "mac_core"|"mac_square")
-    cd "${MAIN_DIR}/ansible" && ansible-playbook --ask-become-pass --ask-vault-pass -i inventories/${INVENTORY} plays/provision/${PLAY}.yml -e "home=${HOME} user_name=${USER_NAME} user_group=${USER_GROUP} homebrew_prefix=${HOMEBREW_DIR} homebrew_install_path=${HOMEBREW_INSTALL_DIR} mas_email=${MAS_EMAIL} mas_password=${MAS_PASSWORD}"
+    cd "${MAIN_DIR}/ansible" && ansible-playbook --ask-become-pass --ask-vault-pass -i inventories/${INVENTORY} plays/provision/${PLAY}.yml -e "home=${HOME} user_name=${USER_NAME} user_group=$(getUserGroup) homebrew_prefix=${HOMEBREW_DIR} homebrew_install_path=${HOMEBREW_INSTALL_DIR} mas_email=${MAS_EMAIL} mas_password=${MAS_PASSWORD}"
     ;;
   "mac_etchost_no_animate"|"mac_jekyll"|"mac_clean")
-    cd "${MAIN_DIR}/ansible" && ansible-playbook --ask-become-pass -i inventories/${INVENTORY} plays/provision/${PLAY}.yml -e "home=${HOME} user_name=${USER_NAME} user_group=${USER_GROUP} homebrew_prefix=${HOMEBREW_DIR} homebrew_install_path=${HOMEBREW_INSTALL_DIR} mas_email=${MAS_EMAIL} mas_password=${MAS_PASSWORD}"
+    cd "${MAIN_DIR}/ansible" && ansible-playbook --ask-become-pass -i inventories/${INVENTORY} plays/provision/${PLAY}.yml -e "home=${HOME} user_name=${USER_NAME} user_group=$(getUserGroup) homebrew_prefix=${HOMEBREW_DIR} homebrew_install_path=${HOMEBREW_INSTALL_DIR} mas_email=${MAS_EMAIL} mas_password=${MAS_PASSWORD}"
     ;;
   "mac_test_full"|"mac_test_short"|"mac_second_account")
-    cd "${MAIN_DIR}/ansible" && ansible-playbook -i inventories/${INVENTORY} plays/provision/${PLAY}.yml -e "home=${HOME} user_name=${USER_NAME} user_group=${USER_GROUP} homebrew_prefix=${HOMEBREW_DIR} homebrew_install_path=${HOMEBREW_INSTALL_DIR} mas_email=${MAS_EMAIL} mas_password=${MAS_PASSWORD}"
+    cd "${MAIN_DIR}/ansible" && ansible-playbook -i inventories/${INVENTORY} plays/provision/${PLAY}.yml -e "home=${HOME} user_name=${USER_NAME} user_group=$(getUserGroup) homebrew_prefix=${HOMEBREW_DIR} homebrew_install_path=${HOMEBREW_INSTALL_DIR} mas_email=${MAS_EMAIL} mas_password=${MAS_PASSWORD}"
     ;;
   *)
-    status e "no matching play for ${PLAY}"
+    LOG e "no matching play for ${PLAY}"
   esac
-  status b "ansible-playbook | ${PLAY} @ ${INVENTORY}"
+  LOG b "ansible-playbook | ${PLAY} @ ${INVENTORY}"
 
   if [ "${ONLY_ANSIBLE}" = false ]; then
     mac_scripts
   fi
 
   # TODO make this an option, not default since if it fails at any point or doesn't have ssh keys then pulling won't work anymore
-  status a "${MAIN_DIR} git remote https:->git:"
+  LOG a "${MAIN_DIR} git remote https:->git:"
   cd ${MAIN_DIR}
   git remote remove origin
   git remote add origin git@github.com:adrw/.files.git
-  status b "${MAIN_DIR} git remote https:->git:"
+  LOG b "${MAIN_DIR} git remote https:->git:"
 
   sudo -k # remove sudo permissions
-  status a "🍺  Fin. Bootstrap Script"
+  LOG a "🍺  Fin. Bootstrap Script"
   exit 0
 }
 
 function linux_bootstrap {
-  status a "Install Linux Base Shell"
+  LOG a "Install Linux Base Shell"
   # ADRW Bash Powerline Theme
   safe_download ~/.adrw-bash-powerline.sh https://raw.githubusercontent.com/adrw/.files/master/ansible/roles/bash/files/.adrw-bash-powerline.sh
   safe_source ~/.adrw-bash-powerline.sh ~/.bashrc
@@ -290,13 +262,14 @@ function linux_bootstrap {
   echo "curl -s https://raw.githubusercontent.com/adrw/.files/master/bootstrap.sh | bash -s" > .ap-update.sh
   chmod +x .ap-update.sh
 
-  status a "🍺  Fin. Bootstrap Script"
+  LOG a "🍺  Fin. Bootstrap Script"
   exit 0
 }
 
+
 bash -c 'figlet -f slant "ADRW .files" 2> /dev/null; echo -n ""'
-status t "Welcome to .files bootstrap!"
-status s "Andrew Paradi. https://github.com/adrw/.files"
+DEBUG "Welcome to ADRW .files"
+DEBUG "https://github.com/adrw/.files"
 
 ONLY_ANSIBLE=false                  # -a
 MAIN_DIR="${HOME}/.files"             # -d
@@ -304,62 +277,99 @@ SCRIPTS="${MAIN_DIR}/scripts"
 HOMEBREW_DIR="${HOME}/.homebrew"      # -b
 HOMEBREW_INSTALL_DIR="${HOMEBREW_DIR}"
 INVENTORY=macbox/hosts              # -i
-LINUX=false                         # -l
 PLAY=mac_core                       # -p
 MAS_EMAIL=                          # -m
 MAS_PASSWORD=                       # -n
 TEST=false                          # -t
 USER_NAME=$(whoami)                 # -u
 SECURE=false                        # -s
+COMPUTER_NAME=$(hostname)
 
-status a "📈  Registered Configuration"
-while getopts "h?ad:b:i:p:m:n:sltu:" opt; do
+function getUserGroup { 
+  id -Gn "$USER_NAME" | cut -d " " -f1 
+}
+
+function processArguments {
+  DEBUG "📈  Registered Configuration"
+  while getopts "h?ad:b:i:p:m:n:stu:" opt; do
     case "$opt" in
     h|\?)
         usage
         exit 0
         ;;
-    a)  echo "  - ONLY_ANSIBLE=true"
+    a)  DEBUG "  - ONLY_ANSIBLE=true"
         ONLY_ANSIBLE=true
         ;;
-    d)  echo "  - MAIN_DIR ${MAIN_DIR} => ${OPTARG}"
+    d)  DEBUG "  - MAIN_DIR ${MAIN_DIR} => ${OPTARG}"
         MAIN_DIR=${OPTARG}
         SCRIPTS="${MAIN_DIR}/scripts"
         ;;
-    b)  echo "  - HOMEBREW_DIR ${HOMEBREW_DIR} => ${OPTARG}"
+    b)  DEBUG "  - HOMEBREW_DIR ${HOMEBREW_DIR} => ${OPTARG}"
         HOMEBREW_DIR=${OPTARG}
         HOMEBREW_INSTALL_DIR="${OPTARG}/Homebrew"
         ;;
-    i)  echo "  - INVENTORY ${INVENTORY} => ${OPTARG}"
+    i)  DEBUG "  - INVENTORY ${INVENTORY} => ${OPTARG}"
         INVENTORY=${OPTARG}
         ;;
-    l)  echo "  - LINUX => PURE (no ansible)"
-        LINUX=true
-        ;;
-    p)  echo "  - PLAY ${PLAY} => ${OPTARG}"
+    p)  DEBUG "  - PLAY ${PLAY} => ${OPTARG}"
         PLAY=${OPTARG}
         ;;
-    m)  echo "  - MAS_EMAIL ${MAS_EMAIL} => ${OPTARG}"
+    m)  DEBUG "  - MAS_EMAIL ${MAS_EMAIL} => ${OPTARG}"
         MAS_EMAIL=$OPTARG
         ;;
-    n)  echo "  - MAS_PASSWORD ${MAS_PASSWORD} => ${OPTARG}"
+    n)  DEBUG "  - MAS_PASSWORD ${MAS_PASSWORD} => ${OPTARG}"
         MAS_PASSWORD=${OPTARG}
         ;;
-    s)  echo "  - Secure network and custom host name"
+    s)  DEBUG "  - Secure network and custom host name"
         SECURE=true
         ;;
-    t)  echo "  - Test Environment (Git Head still attached)"
+    t)  DEBUG "  - Test Environment (Git Head still attached)"
         TEST=true
         ;;
-    u)  echo "  - USER ${USER_NAME} => ${OPTARG}"
+    u)  DEBUG "  - USER ${USER_NAME} => ${OPTARG}"
         USER_NAME=${OPTARG}
         HOME="/Users/${USER_NAME}"
         ;;
     esac
-done
+  done
 
-shift $((OPTIND-1))
-echo "Leftovers: $@"
+  shift $((OPTIND-1))
+  DEBUG "Leftovers: $*"
+}
+
+function interactiveArguments {
+  # read -p "Next test? [y/n/enter] " -n 1 -r && echo ""
+  DEBUG "Answer the 14 questions below to build your custom install"
+
+  # Secure Network
+  DEBUG "# Secure your Computer Name and Network Settings?"
+  DEBUG "Change your computer name from ${COMPUTER_NAME}, turn on firewall, randomize MAC address"
+  read -p "[Enter] to skip. Type to run with new computer name: " -r Q_COMPUTER_NAME
+  if [[ $Q_COMPUTER_NAME != "" ]]; then
+    COMPUTER_NAME=$Q_COMPUTER_NAME
+    secure_hostname_network
+  else
+    DEBUG "Skipping..."
+  fi
+
+  # User
+  DEBUG "# Run as User: ${USER_NAME}?"
+  read -p "[Enter] to skip. Type to overwrite: " -r Q_USER_NAME
+  if [[ $Q_USER_NAME != "" ]]; then
+    USER_NAME=$Q_USER_NAME
+    WARN "Updated User: ${USER_NAME}"
+  fi
+
+  cd "${MAIN_DIR}/ansible" && ansible-playbook --ask-become-pass --ask-vault-pass -i inventories/${INVENTORY} plays/provision/${PLAY}.yml -e "home=${HOME} user_name=${USER_NAME} user_group=${USER_GROUP} homebrew_prefix=${HOMEBREW_DIR} homebrew_install_path=${HOMEBREW_INSTALL_DIR} mas_email=${MAS_EMAIL} mas_password=${MAS_PASSWORD}"
+
+  
+}
+
+if [ $# -eq 0 ]; then
+  interactiveArguments
+else
+  processArguments "$@"
+fi
 
 if [[ ${SECURE} == true ]]; then
   secure_hostname_network
@@ -378,5 +388,5 @@ case "$(uname)" in
               ;;
 esac
 
-status e "Unknown Error. Maybe invalid platform (Only works on Mac or Linux)."
+FATAL "Unknown Error. Maybe invalid platform (Only works on Mac or Linux)."
 exit 1
---
+++ docs/audiobook.md
@@ -0,0 +1,27 @@
+Reformatting Audio Books for iPod
+---
+
+- Use `brew cask install mediahuman-audio-converter` to compress the files with the following settings
+  - 24kbps
+  - Mono
+  - AAC Audio
+  - m4b file
+  - 22.050 khz sample
+- Rename files in Finder to a author/book identifier followed by counter to signify part number. A reduction in length allows it to appear on the iPod screen.
+
+  ```
+  12 Rules for Life An Antidote to Chaos (Unabridged) - 01.m4a
+
+  ->
+
+  JBP 12 Rules 00001.m4a
+  ```
+
+- Use `zmv` to rename all files to m4b to enable Audiobook functionality like progress tracking
+  - Example command `zmv '*.m4a' '\$(basename \$f .m4a).m4b'`
+- Use `brew cask install musicbrainz-picard` to set the file metadata using the filename
+  - `Tools -> Tags from File Names`
+  - Manually set Artist, Album to ensure consistency
+  - Remove unnecessary additional fields including Track Number
+  - `Save` to overwrite the metadata in the tracks
+- Import into iTunes, the `m4b` extension should automatically imply the Audiobook type
